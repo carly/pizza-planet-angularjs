08acf4179b3c76d25703b48cb4f9f5f05aac1890
carobinson <carly.a.robinson@gmail.com>
2015-02-28 16:14:41 -0500

    initial commit

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,6 +11,45 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    }
+  ];
+  $scope.mains = [
+    {
+      name: 'Margarita',
+      description: 'Marinara sauce, mozzarella, fresh basil.',
+      price: 7.95
+    },
+    {
+      name: 'Pepperoni',
+      description: 'Pepperoni, marinara sauce, mozzarella, fresh oregano.',
+      price: 8.95
+    },
+    {
+      name: 'Rustica',
+      description: 'Marinara sauce, kalamata olives, roasted garlic, fresh oregano.',
+      price: 8.95
+    }
+  ];
+  $scope.extras = [
+    {
+      name: 'Breadsticks',
+      description: 'Served with marinara sauce.',
+      price: 4.95
+    },
+    {
+      name: 'Soup of the day',
+      description: 'Take a chance.',
+      price: 4.95
+    },
+    {
+      name: 'Buffalo Wings',
+      description: 'Feel the power of wings.',
+      price: 6.95
     }
   ];
 
